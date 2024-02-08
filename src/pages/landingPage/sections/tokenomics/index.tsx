26e0d3a4941404fcf55c37f48559c35ad4caaf6d
Favour Dgreat <53022031+Favour-dgreat@users.noreply.github.com>
2024-02-08 01:55:53 +0100

    isPaused fixed

--- src/pages/landingPage/sections/tokenomics/index.tsx
+++ src/pages/landingPage/sections/tokenomics/index.tsx
@@ -18,6 +18,7 @@ import 'slick-carousel/slick/slick-theme.css';
 // import { ArrowUp } from 'react-bootstrap-icons';
 
 
+
 const ThisYear = () => {
   const [isPaused, setIsPaused] = useState(false);
 
@@ -34,82 +35,68 @@ const ThisYear = () => {
     beforeChange: () => setIsPaused(true),
     afterChange: () => setIsPaused(false),
   };
-    return (
-        <>
-          {/* <div id="story" className={styles.story_container}> */}
-          <Image
-            data-aos="fade-up"
-            data-aos-duration="2000"
-            data-aos-anchor-placement="center-bottom"
-            src={thisyearImg1}
-            className={styles.story_item_image3}
-            style={{ flexShrink: 0, height: '100%', width: 'auto', }}
-          />
+
+  return (
+    <>
+      <Image
+        data-aos="fade-up"
+        data-aos-duration="2000"
+        data-aos-anchor-placement="center-bottom"
+        src={thisyearImg1}
+        className={styles.story_item_image3}
+        style={{ flexShrink: 0, height: '100%', width: 'auto' }}
+      />
       <div id="story" className={styles.story_container}>
-     
-      <div className={styles.story_inner}>
-      <div className={styles.card}>
-        <div className={styles.icon}>
-        <Icon.ArrowUpSquareFill color="#000" size={56} className={styles.gi}/>
+        <div className={styles.story_inner}>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Icon.ArrowUpSquareFill color="#000" size={56} className={styles.gi}/>
+            </div>
+            <p className={styles.tt}>Total Supply</p>
+            <p className={styles.tt}>10,000</p>
+            <p className={styles.marginBottom}> </p>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Icon.CartCheckFill color="#000" size={56} className={styles.gi}/>
+            </div>
+            <p className={styles.tt}>Sell Tax</p>
+            <p className={styles.tt}>0</p>
+            <p className={styles.marginBottom}> </p>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.icon}>
+              <Icon.CartPlusFill color="#000" size={56} className={styles.gi}/>
+            </div>
+            <p className={styles.tt}>Buy Tax</p>
+            <p className={styles.tt}>0</p>
+            <p className={styles.marginBottom}> </p>
+          </div>
         </div>
-        <p className={styles.tt}>Total Supply</p>
-        <p className={styles.tt}>10,000</p>
-        <p className={styles.marginBottom}> </p>
-
+        <div className={styles.carousel}>
+          <Slider {...settings}>
+            <div>
+              <img src={thisyearImg2} alt="Image 1" className={styles.slider} />
+            </div>
+            <div>
+              <img src={thisyearImg3} alt="Image 2" className={styles.slider} />
+            </div>
+            <div>
+              <img src={thisyearImg4} alt="Image 3" className={styles.slider} />
+            </div>
+            <div>
+              <img src={thisyearImg5} alt="Image 4" className={styles.slider} />
+            </div>
+            <div>
+              <img src={thisyearImg6} alt="Image 5" className={styles.slider} />
+            </div>
+          </Slider>
         </div>
-<div className={styles.card}>
-        <div className={styles.icon}>
-
-        <Icon.CartCheckFill color="#000" size={56} className={styles.gi}/>
-        </div>
-        <p className={styles.tt}>Sell Tax</p>
-        <p className={styles.tt}>0</p>
-        <p className={styles.marginBottom}> </p>
-</div>
-<div className={styles.card}>
-        <div className={styles.icon}>
-
-        <Icon.CartPlusFill color="#000" size={56} className={styles.gi}/>
-        </div>
-        <p className={styles.tt}>Buy Tax</p>
-        <p className={styles.tt}>0</p>
-        <p className={styles.marginBottom}> </p>
-</div>
       </div>
-      <div className={styles.carousel}>
-      <Slider {...settings}>
-          <div>
-            <img src={thisyearImg2} alt="Image 1"className={styles.slider}/>
-          </div>
-          <div>
-          <img src={thisyearImg3} alt="Image 1"className={styles.slider}/>
-          </div>
-          <div>
-          <img src={thisyearImg4} alt="Image 1"className={styles.slider}/>
-          </div>
-          <div>
-          <img src={thisyearImg5} alt="Image 1"className={styles.slider}/>
-          </div>
-          <div>
-          <img src={thisyearImg6} alt="Image 1"className={styles.slider}/>
-          </div>
-        </Slider>
-      </div>
-    </div>
-    
-      {/* </div> */}
-      {/* <Image
-            data-aos="fade-up"
-            data-aos-duration="2000"
-            data-aos-anchor-placement="center-bottom"
-            src={storyImgIcon}
-            className={styles.story_item_image2}
-            style={{ flexShrink: 0, width: 'auto', height: '100%' }}
-          /> */}
-    
-        </>
-      );
-    }
-    
+    </>
+  );
+};
 
 export default ThisYear;
+
+    
