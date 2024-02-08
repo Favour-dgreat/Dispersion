f4705d8b3fb9496cafd0965c148f8959fbb427be
Umoren Daniel <femidu35@gmail.com>
2024-02-08 20:48:00 +0100

    update

--- src/pages/landingPage/sections/features/index.tsx
+++ src/pages/landingPage/sections/features/index.tsx
@@ -3,8 +3,38 @@ import Image from '../../../../components/images';
 import thisfeature2Img from '../../../../assets/icons/Group 77.png';
 import thisfeature3Img from '../../../../assets/icons/11.png';
 import { RegularButton } from '../../../../components/buttons';
+import thisyearImg2 from '../../../../assets/pictures/z1.png';
+import thisyearImg3 from '../../../../assets/pictures/z4.png';
+import thisyearImg4 from '../../../../assets/pictures/z5.png';
+import thisyearImg5 from '../../../../assets/pictures/z6.png';
+import thisyearImg6 from '../../../../assets/pictures/z7.png';
+
+
+// import ReactDOM from 'react-dom'
+import { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+// import { ArrowUp } from 'react-bootstrap-icons';
+
+ 
 
 const ThisYear = () => {
+    const [,setIsPaused] = useState(false);
+
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 1000, // Change the speed of sliding
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000, // Change the speed of autoplay
+      pauseOnHover: true,
+      pauseOnFocus: true,
+      beforeChange: () => setIsPaused(true),
+      afterChange: () => setIsPaused(false),
+    };
     return (
         <>
           {/* <div id="story" className={styles.story_container}> */}
@@ -41,6 +71,25 @@ const ThisYear = () => {
 
 
       </div>
+      <div className={styles.carousel}>
+          <Slider {...settings}>
+            <div>
+              <img src={thisyearImg2} alt="Image 1" className={styles.slider} />
+            </div>
+            <div>
+              <img src={thisyearImg3} alt="Image 2" className={styles.slider} />
+            </div>
+            <div>
+              <img src={thisyearImg4} alt="Image 3" className={styles.slider} />
+            </div>
+            <div>
+              <img src={thisyearImg5} alt="Image 4" className={styles.slider} />
+            </div>
+            <div>
+              <img src={thisyearImg6} alt="Image 5" className={styles.slider} />
+            </div>
+          </Slider>
+        </div>
      
     </div>
     
--- src/pages/landingPage/sections/tokenomics/index.tsx
+++ src/pages/landingPage/sections/tokenomics/index.tsx
@@ -2,40 +2,18 @@ import styles from './thisyear.module.css';
 import Image from '../../../../components/images';
 // import thisyearImg from '../../../../assets/pictures/ellipse.png';
 import thisyearImg1 from '../../../../assets/icons/token.png';
-import thisyearImg2 from '../../../../assets/pictures/z1.png';
-import thisyearImg3 from '../../../../assets/pictures/z4.png';
-import thisyearImg4 from '../../../../assets/pictures/z5.png';
-import thisyearImg5 from '../../../../assets/pictures/z6.png';
-import thisyearImg6 from '../../../../assets/pictures/z7.png';
+
 
 
 // import ReactDOM from 'react-dom'
 import * as Icon from 'react-bootstrap-icons';
-import { useState } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
 // import { ArrowUp } from 'react-bootstrap-icons';
 
 
 
 const ThisYear = () => {
-  const [,setIsPaused] = useState(false);
-
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 1000, // Change the speed of sliding
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000, // Change the speed of autoplay
-    pauseOnHover: true,
-    pauseOnFocus: true,
-    beforeChange: () => setIsPaused(true),
-    afterChange: () => setIsPaused(false),
-  };
-
+  
   return (
     <>
       <Image
